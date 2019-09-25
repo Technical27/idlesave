d5c22f4496ff2c0eb74314d7293b4fa6daef26c4
Technical27 <38222826+Technical27@users.noreply.github.com>
2019-09-25 08:42:58 -0400

    added ability to edit level

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let money, gold, parts;
 
 const parseSave = s => {
   parts = atob(s).split(',');
+  level = parts[0];
   money = parts[1];
   gold = parts[2];
 }
@@ -16,10 +17,12 @@ $(document).ready(() => {
     parseSave($('#save').val());
     $('#gold').val(gold);
     $('#money').val(money);
+    $('#level').val(level);
   });
   $('#gen').click(() => {
     if (!parts) return;
     $('#genText').show();
+    parts[0] = $('#level').val();
     parts[1] = $('#money').val();
     parts[2] = $('#gold').val();
     $('#genSave').html(genSave());
